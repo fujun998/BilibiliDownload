c151497d698ad91b97679204db43d04d89f2d036
fujun998 <fujun998@outlook.com>
2022-03-13 17:43:08 +0800

    a

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿//using System.Net.Http;
+using HttpClient client = new();
+Console.WriteLine(await client.GetStringAsync("https://api.bilibili.com/x/player/playurl?avid=969628065&cid=244954665&qn=127&fnval=80&fnver=0&fourk=1"));
