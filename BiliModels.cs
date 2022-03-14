ef04df3c8ef6e6c93aac2938245c8d21dccb5417
fujun998 <61316972+fujun998@users.noreply.github.com>
2022-03-15 01:07:32 +0800

    model

--- BiliModels.cs
+++ BiliModels.cs
@@ -1,14 +1,18 @@
 using System.Text.Json.Serialization;
 
-record BiliSeason(BiliEpisode[] Episodes, string Title);
-
 record BiliVideo(string Title, string BVId, long Cid, BiliVideoDimension Dimension, BiliVideoPage[] Pages);
 
 record BiliVideoPage(long Cid, int Page, BiliVideoDimension Dimension, string Part);
 
+
+record BiliSeason(BiliEpisode[] Episodes, string Title);
+
 record BiliEpisode(string Title, string BVId, long Cid, BiliVideoDimension Dimension, string LongTitle);
 
 record struct BiliVideoDimension(int Width, int Height, int Rotate);
+
+
+record BiliVideoPlayUrl(BiliVideoDashInfo Dash);
 
 record BiliVideoDashInfo(BiliDashUrl[] Video, BiliDashUrl[] Audio);
 
