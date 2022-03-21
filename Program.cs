4214695659bc1ade647b6b9971db3a9a24e4f841
fujun998 <61316972+fujun998@users.noreply.github.com>
2022-03-21 10:38:45 +0800

    asds

--- Program.cs
+++ Program.cs
@@ -1,18 +1,20 @@
 ﻿var ss = await BiliApis.GetSeasonInfo(40156);
-//Console.WriteLine(ss);
+//Console.WriteLine(ss.RawJson);
 foreach (var ep in ss.Episodes)
 {
     //Console.WriteLine(ep);
 }
 
-var vid = await BiliApis.GetVideoInfo("BV1rp4y1e745");
-Console.WriteLine(vid);
+//var vid = await BiliApis.GetVideoInfo("BV1rp4y1e745");
+var vid = await BiliApis.GetVideoInfo("BV1XR4y1F7ZQ");
 
-var playurl = await BiliApis.GetPlayUrlAsync("BV1rp4y1e745", 244954665);
+//var playurl = await BiliApis.GetPlayUrlAsync("BV1rp4y1e745", 244954665);
+var playurl = await BiliApis.GetPlayUrlAsync("BV1XR4y1F7ZQ", 553981907);
+File.WriteAllText("Sample-AV1.json", playurl.RawJson);
 foreach (var dash in playurl.Dash.Video)
 {
     //Console.WriteLine(dash);
 }
 
-var stream = await BiliHttpClient.GetStreamAsync(playurl.Dash.Video.Last().BaseUrl);
-stream.CopyTo(File.OpenWrite("output/o.mp4"));
+//var stream = await BiliHttpClient.GetStreamAsync(playurl.Dash.Video.Last().BaseUrl);
+//stream.CopyTo(File.OpenWrite("output/o.mp4"));
